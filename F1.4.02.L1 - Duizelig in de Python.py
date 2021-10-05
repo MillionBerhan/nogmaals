5b1073eec752232afebbbc1a820a2a01c6ae4efa
MILLION BERHAN <99065113@mydavinci.com>
2021-10-05 11:58:21 +0200

    F1.4.02.L1 - Duizelig in de Python

---
+++ F1.4.02.L1 - Duizelig in de Python.py
@@ -0,0 +1,68 @@
+print ("welkom bij het sofware Berhano!")
+
+i = 30
+while i >= 0:
+  print(i)
+  i -= 1
+print("lancering\n")
+
+input("Press Enter to continue...") 
+
+print ("AM hours\n")
+a = 0
+while a <12:
+  a+=1
+  print (str(a) + "AM\n")
+
+print ("PM hours\n")
+b = 0
+while a <12:
+  b+=1
+  print (str(b) + "PM\n")
+
+input("Press Enter to continue...") 
+
+j = 20
+while j <= 50:
+  print (j)
+  j += 2
+  
+input("Press Enter to continue...")   
+
+dagen = input ("welke dag van de week kiest u uit?")
+if dagen == "ma":
+    print ("ma")
+if dagen == "di":
+    print ("ma en di")
+if dagen == "wo":
+    print ("ma, di en wo")
+if dagen == "do":
+    print ("ma,di,wo en do")
+if dagen == "vri":
+    print ("ma,di,wo,do en vri")
+if dagen == "za":
+    print ("ma,di,wo,do,vr,za")
+if dagen == "zo":
+    print ("ma,di,wo,do,vr,za,zo")
+
+input("Press Enter to continue...")      
+
+while input('Zeg quit!\n') != 'quit':
+    print('quit')
+
+print('Je hebt het gezegd')
+
+input("Press Enter to continue...")  
+
+j = 50
+while j <= 1000:
+  print (j)
+  j += 1
+
+print ("Bedankt voor het testen van Software Berhano!")
+
+input("Press Enter to exit...")  
+
+
+
+
